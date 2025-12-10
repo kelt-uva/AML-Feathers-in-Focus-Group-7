6e1fabd5684ffa02cbddcdebdd9f3c9e62cacb2a
Vivien <you@example.com>
2025-12-10 19:49:17 +0100

    added best training data to the results of hyperparameter tuning

--- src/parameter_tuning.py
+++ src/parameter_tuning.py
@@ -16,27 +16,34 @@ def run():
     parameter_weight_decay = []
     accuracies = []
     f1_scores = []
+    train_accuracies = []
+    train_f1_scores = []
 
     batch_sizes = [32]
-    learning_rates = [1e-2, 1e-3, 5e-4, 1e-4, 5e-5]
-    weight_decays = [1e-2, 1e-3, 5e-4, 1e-4]
-
+    learning_rates = [1e-2, 1e-3, 5e-4, 1e-4, 5e-5, 1e-5]
+    #learning_rates = [5e-5]
+    weight_decays = [1e-2, 1e-3, 5e-4, 1e-4, 5e-5, 1e-5]
+    weight_decays = [1e-2]
     for batch_size in batch_sizes:
         for learning_rate in learning_rates:
             for weight_decay in weight_decays:
                 print(f"Running training with batch_size={batch_size}, learning_rate={learning_rate}, weight_decay={weight_decay}")
-                acc, f1 = main(batch_size=batch_size, learning_rate=learning_rate, weight_decay=weight_decay)
+                train_acc, train_f1, acc, f1 = main(batch_size=batch_size, learning_rate=learning_rate, weight_decay=weight_decay)
                 parameter_batch_size.append(batch_size),
                 parameter_learning_rate.append(learning_rate),
                 parameter_weight_decay.append(weight_decay),
                 accuracies.append(acc)
                 f1_scores.append(f1)
+                train_accuracies.append(train_acc)
+                train_f1_scores.append(train_f1)
 
     results['Batch size'] = parameter_batch_size
     results['Learning rate'] = parameter_learning_rate
     results['Weight decay'] = parameter_weight_decay
     results['Validation Accuracy'] = accuracies
     results['Validation F1 Score'] = f1_scores
+    results['Training Accuracy'] = train_accuracies
+    results['Training F1 Score'] = train_f1_scores
     results_df = pd.DataFrame(results)
     results_df.to_csv(save_path, index=True)
 
