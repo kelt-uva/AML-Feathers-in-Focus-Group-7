8970362aa6fed516d5860a646e4ef00c1d9232bc
Vivien <you@example.com>
2025-12-12 13:37:15 +0100

    added models for comparision

--- src/model.py
+++ src/model.py
@@ -59,6 +59,136 @@ def create_efficientnet(num_classes, model_name = "efficientnet_b0", pretrained 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
+def create_simple_model():
+
+    simple_model = nn.Sequential(
+        # layer 1
+        nn.Conv2d(3, 16, kernel_size = 3, padding = 1),
+        nn.BatchNorm2d(16),
+        nn.ELU(),
+        nn.MaxPool2d(kernel_size = 2),
+        # layer 2
+        nn.Conv2d(16, 32, kernel_size = 3, padding = 1),
+        nn.BatchNorm2d(32),
+        nn.ELU(),
+        nn.MaxPool2d(kernel_size = 2),
+        # layer 3
+        nn.Conv2d(32, 64, kernel_size = 3, padding = 1),
+        nn.BatchNorm2d(64),
+        nn.ELU(),
+        nn.MaxPool2d(kernel_size = 2),
+        nn.Flatten(),
+        nn.Linear(64 * 28 * 28, 200)
+    )
+
+    return simple_model
+
+def create_deep_wide():
+    deep_wide_model = nn.Sequential(
+        # layer 1
+        nn.Conv2d(3, 16, kernel_size=3, padding=1),
+        nn.BatchNorm2d(16),
+        nn.ELU(),
+        nn.MaxPool2d(kernel_size = 2),
+
+        # layer 2
+        nn.Conv2d(16, 32, kernel_size=3, padding=1),
+        nn.BatchNorm2d(32),
+        nn.ELU(),
+        nn.MaxPool2d(kernel_size = 2),
+
+        # layer 3
+        nn.Conv2d(32, 64, kernel_size=3, padding=1),
+        nn.BatchNorm2d(64),
+        nn.ELU(),
+        nn.MaxPool2d(kernel_size = 2),
+
+        # layer 4
+        nn.Conv2d(64, 128, kernel_size=3, padding=1),
+        nn.BatchNorm2d(128),
+        nn.ELU(),
+        nn.MaxPool2d(kernel_size = 2),
+
+        # layer 5
+        nn.Conv2d(128, 256, kernel_size=3, padding=1),
+        nn.BatchNorm2d(256),
+        nn.ELU(),
+        nn.MaxPool2d(kernel_size = 2),
+
+        # layer 6
+        nn.Conv2d(256, 512, kernel_size=3, padding=1),
+        nn.BatchNorm2d(512),
+        nn.ELU(),
+        nn.AdaptiveMaxPool2d((3, 3)),
+
+        # Linear layer
+        nn.Flatten(),
+        nn.Linear(512 * 3 * 3, 512),
+        nn.ELU(),
+        nn.Linear(512, 200),
+    )
+
+    return deep_wide_model
+
+def create_vgg():
+
+    vgg_model = nn.Sequential(
+
+        # convolutional block 1
+        nn.Conv2d(3, 64, kernel_size = 3, padding = 1),
+        nn.ReLU(),
+        nn.Conv2d(64, 64, kernel_size=3, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(2),
+
+        # convolutional block 2
+        nn.Conv2d(64, 128, kernel_size=3, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(128, 128, kernel_size=3, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(2),
+
+        # convolutional block 3
+        nn.Conv2d(128, 256, kernel_size=3, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(256, 256, kernel_size=3, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(2),
+
+        # convolutional block 4
+        nn.Conv2d(256, 512, kernel_size=3, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(512, 512, kernel_size=3, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(512, 512, kernel_size=3, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(2),
+
+        # convolutional block 5
+        nn.Conv2d(512, 512, kernel_size=3, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(512, 512, kernel_size=3, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(512, 512, kernel_size=3, padding=1),
+        nn.ReLU(),
+        nn.AdaptiveMaxPool2d((3, 3)),
+
+        # fully connected layers
+        nn.Flatten(),
+        nn.Linear(512*3*3, 512),
+        nn.Dropout(0.5),
+        nn.ReLU(),
+        nn.Linear(512, 512),
+        nn.Dropout(0.5),
+        nn.ReLU(),
+        nn.Linear(512, 200)
+    )
+
+    return vgg_model
+    
+
+
+
 
 def create_own_model():
     # this is the new model
@@ -66,6 +196,7 @@ def create_own_model():
     new_model = nn.Sequential(
         # layer 1
         nn.Conv2d(3, 16, kernel_size = 3, padding = 1),
+        nn.BatchNorm2d(16),
         nn.ELU(),
         nn.MaxPool2d(kernel_size = 2),
         # layer 2
@@ -86,6 +217,7 @@ def create_own_model():
         nn.Dropout(0.3),
         # layer 5
         nn.Conv2d(128, 256, kernel_size = 3, padding = 1),
+        nn.BatchNorm2d(256),
         nn.ELU(),
         nn.MaxPool2d(kernel_size = 2),
         # Bottleneck layer
@@ -102,6 +234,7 @@ def create_own_model():
         nn.Conv2d(256, 512, kernel_size = 3, padding = 1),
         nn.ELU(),
         nn.AdaptiveMaxPool2d((3, 3)),
+        # Linear layers
         nn.Dropout(0.4),
         nn.Flatten(),
         nn.Linear(512 * 3 * 3, 512),
